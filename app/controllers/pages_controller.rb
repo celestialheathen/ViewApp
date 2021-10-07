0bcfefded6602c5b751b3ea6d8f831fa04dc8925
celestialheathen <jingchaoyu@hotmail.com>
2021-10-07 12:56:13 -0400

    Added routes for different food categories and controller actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,29 @@ class PagesController < ApplicationController
   def home 
     @items = Item.all
   end 
+
+  def beef 
+    @beefs = Item.where("category @> ?", '{beef}')
+  end 
+
+  def chicken
+    @chickens = Item.where("category @> ?", '{chicken}')
+  end 
+
+  def vegetable 
+    @vegetables = Item.where("category @> ?", '{vegetable}')
+  end 
+
+  def seafood 
+    @seafoods = Item.where("category @> ?", '{seafood}')
+  end 
+
+  def appetizer 
+    @appetizers = Item.where("category @> ?", '{appetizer}')
+  end 
+
+  def special 
+    @specials = Item.where("category @> ?", '{lunch special}')
+  end 
   
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
 
   get 'ca', to: 'locations#canada'
 
+  root to: 'pages#home'
+  get 'beef', to: 'pages#beef'
+  get 'chicken', to: 'pages#chicken'
+  get 'vegetable', to: 'pages#vegetable'
+  get 'seafood', to: 'pages#seafood'
+  get 'appetizer', to: 'pages#appetizer'
+  get 'special', to: 'pages#special'
+
 end
